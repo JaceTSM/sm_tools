6439b1283e985ff31f680c7932d663e0323804ab
Tim Murphy <jac3tssm@gmail.com>
2021-04-28 13:39:06 -0400

    update minor version: 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="sm_tools",
-    version="0.0.1",
+    version="0.0.2",
     author="Tim Murphy",
     author_email="jac3tssm@gmail.com",
     description="StepMania tools",
